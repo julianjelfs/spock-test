6ebd310b5acbaebd078ea4f663a81b82dea6aee9
Julian Jelfs <julian.jelfs@gmail.com>
2018-05-13 12:34:42 +0100

    started on aeson parser for config - seems to work

--- app/Api.hs
+++ app/Api.hs
@@ -3,17 +3,19 @@
 
 module Api where
 
+import Control.Exception as E
 import Control.Lens
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Reader
-import Data.Aeson (Value)
-
---import Data.Aeson.Lens (_String, key)
+import qualified Data.Aeson as A
+import qualified Data.Aeson.Types as A
+import Data.Aeson.Types ((.:))
+import qualified Data.ByteString.Lazy as BS
 import Data.Map as Map
 import qualified Data.Text as T
-import Network.Wreq
+import qualified Network.Wreq as W
 
-type Resp = Response (Map String Value)
+type Resp = W.Response (Map String A.Value)
 
 data Endpoints = Endpoints
   { _config :: String
@@ -39,25 +41,28 @@ makeLenses ''Endpoints
 
 type App a = ReaderT Env IO a
 
-opts :: App Options
+opts :: App W.Options
 opts = do
   (d, p) <- extract <$> ask
-  let domain = param "domainId" .~ [parse d]
-      pos = param "posId" .~ [parse p]
-      labels = param "labels" .~ ["GraphQL"]
-  pure $ defaults & (domain . pos . labels)
+  let domain = W.param "domainId" .~ [parse d]
+      pos = W.param "posId" .~ [parse p]
+      labels = W.param "labels" .~ ["GraphQL"]
+  pure $ W.defaults & (domain . pos . labels)
   where
     parse = T.pack . show
     extract e = (e ^. userContext . domainId, e ^. userContext . posId)
 
-fetchConfig :: App (Map String Value)
+fetchConfig :: App (Maybe Config)
 fetchConfig = do
   url <- configUrl <$> ask
   o <- opts
-  resp <- liftIO $ asJSON =<< getWith o url
-  pure $ resp ^. responseBody
+  resp <- liftIO (W.getWith o url `E.catch` onError)
+  pure $ A.decode $ resp ^. W.responseBody
   where
     configUrl e = e ^. endpoints . config
+    onError e@(SomeException s) = do
+      putStrLn "Not sure how we would go about handling this"
+      throwIO e
 
 getConfig = runReaderT fetchConfig defaultEnv
 
@@ -71,3 +76,19 @@ defaultEnv =
           , _someOther = ""
           }
     }
+
+--data structure for actually holding the config
+data Config = Config
+  { publishedVersion :: Integer
+  , version :: Integer
+  } deriving (Show)
+
+instance A.FromJSON Config where
+  parseJSON = configParser
+
+configParser :: A.Value -> A.Parser Config
+configParser =
+  A.withObject "config" $ \obj -> do
+    version <- obj .: "Version"
+    publishedVersion <- obj .: "PublishedVersion"
+    pure Config {publishedVersion = publishedVersion, version = version}
